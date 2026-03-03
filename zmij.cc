7e41af5e73b6516ad8f788d7a8ddb0734b373e32
Tobias Schlüter <tobi@schlueters.de>
2026-03-03 17:41:30 +0900

    Write four bytes.
    Safe and less uops than writing one byte.

--- zmij.cc
+++ zmij.cc
@@ -1028,7 +1028,8 @@ auto write_fixed_double_sse4(char* buffer, uint64_t dec_sig, int dec_exp,
   auto digits_with_dot = _mm_blendv_epi8(digits, dots, shuffler);
 
   _mm_storeu_si128(reinterpret_cast<__m128i*>(buffer), digits_with_dot);
-  buffer[16] =  (char)_mm_cvtsi128_si64(unshuffled_digits);
+  uint32_t trailing_digit = _mm_cvtsi128_si32(unshuffled_digits);
+  memcpy(buffer + 16, &trailing_digit, 4); // only need the lowest byte
 
   char* point = buffer + dec_exp + !extra_digit;
   buffer += len;
